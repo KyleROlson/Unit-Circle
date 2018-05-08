411398f6dcd4ee5726ce066a92037012e305ab8b
Kyle Olson <kylerolson@hotmail.com>
2018-05-08 03:35:42 +0000

    Version 0.5

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <link rel="manifest" href="manifest.json">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Unit Circle</title>
+</head>    
+    <body>
+       <header><br><center><img src="images/Logo.png"></center></header>
+       <center>
+          <a href="menu.php"><div id="playButton"><br><img width="30%" src="images/playText.png"></div></a>
+           <br>
+           <br>
+           <div id="highButton"><br><br><img width="80%" src="images/highScores.png"></div>
+       </center>
+       <footer><img  width ="100%"src="images/footer.png"></footer>
+    </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    
+    <link rel="manifest" href="manifest.json">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Unit Circle</title>
+</head>    
+    <body onLoad="timer()">
+       <header><img src="images/arrow.png"></header>
+       <center>
+           <a href="UnitCircle.php" ><div id="playButton"><br><img width="80%" src="images/ucm.png"></div></a>
+           <br>
+           <br>
+           <a href="FlashCards.php" ><div id="highButton"><br><br><img width="80%" src="images/fcm.png"></div></a>
+       </center>
+       
+    </body>
+</html>
